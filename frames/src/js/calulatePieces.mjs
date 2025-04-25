a539b4dcd196f12436e404b364e8dbe83d26a04b
fckolob <fckolob@gmail.com>
2025-04-24 21:49:59 -0300

    methods added to class opening and to calculatePieces

--- frames/src/js/calulatePieces.mjs
+++ frames/src/js/calulatePieces.mjs
@@ -1,5 +1,12 @@
 export function s20(width, height, quantity){
-    const pieces = {horizontalFrame: width-25, verticalFrame: height, lateralShash: height-48, centralShash: height-48, horizontalShash: width / 2 - 74, glassWidth: width / 2 - 58, glassHeight: height - 112}
+    const pieces = {horizontalFrame: {lenght: width - 25, quantity: quantity * 2}, verticalFrame: {lenght: height, quantity: quantity * 2}, lateralShash: {lenght: height-48, quantity: quantity * 2}, centralShash: {lenght: height-48, quantity: quantity * 2}, horizontalShash: {lenght: width / 2 - 74, quantity: quantity * 4}, glassWidth: {lenght: width / 2 - 58, quantity: quantity * 2}, glassHeight: {lenght: height - 112, quantity: quantity * 2}, screenWidth: {lenght: width / 2 + 10, quantity: quantity * 2}, screenHeight: {lenght: height - 30, quantity: quantity * 2}};
+    console.log(pieces);
     return pieces;
     
+}
+
+export function s25(width, height, quantity){
+    const pieces = {horizontalFrame: {lenght: width - 51, quantity: quantity * 2}, verticalFrame: {lenght: height, quantity: quantity * 2}, lateralShash: {lenght: height-48, quantity: quantity * 2}, centralShash: {lenght: height-48, quantity: quantity * 2}, horizontalShashBig: {lenght: width / 2 - 96, quantity: quantity * 2}, horizontalShashSmall: {lenght: width / 2 - 96, quantity: quantity * 2}, glassWidth: {lenght: width / 2 - 58, quantity: quantity * 2}, glassHeight: {lenght: height - 112, quantity: quantity * 2}, screenWidth: {lenght: width / 2 + 10, quantity: quantity * 2}, screenHeight: {lenght: height - 30, quantity: quantity * 2}};
+    console.log(pieces);
+    return pieces;
 }
--- frames/src/js/opening.mjs
+++ frames/src/js/opening.mjs
@@ -1,3 +1,8 @@
+import { s20, s25 } from "./calulatePieces.mjs";
+import Frame from "./frame.mjs";
+
+
+
 export default class Opening{
 
 constructor(width, height, serie, color, dvh, preframe, quantity){
@@ -8,10 +13,67 @@ constructor(width, height, serie, color, dvh, preframe, quantity){
     this.dvh = dvh;
     this.preframe = preframe;
     this.quantity = quantity;
+    this.pieces = {};
+    this.frames = {};
+    this.glass = {};
+
 }
 
-calculatePieces(){
-    
+#calculatePieces(){
+   if (this.serie === "s20"){
+    this.pieces = s20(this.width, this.height, this.quantity);
+   }
+
+   if (this.serie === "25"){
+    this.pieces = s25(this.width, this.height, this.quantity);
+   }
 }
+
+framing(){
+  this.#calculatePieces();
+    if (this.serie === "s20"){
+
+        const horizontalFrame = new Frame(this.serie, {abasur: "N1749", urualum: "190", juan: "204 o 190", aluminiosDelUruguay: "PN 0190"}, this.pieces.horizontalFrame.lenght, "Horizontal Frame", this.color, this.pieces.horizontalFrame.quantity);
+
+        const verticalFrame = new Frame(this.serie, {abasur: "N1753", urualum: "191", juan: "205 o 191", aluminiosDelUruguay: "PN 0191"}, this.pieces.verticalFrame.lenght, "Horizontal Frame", this.color, this.pieces.verticalFrame.quantity);
+
+        const lateralShash = new Frame(this.serie, {abasur: "N1751", urualum: "193", juan: "202 o 193", aluminiosDelUruguay: "PN 0193"}, this.pieces.lateralShash.lenght, "Horizontal Frame", this.color, this.pieces.lateralShash.quantity);
+
+        const centralShash = new Frame(this.serie, {abasur: "E2864", urualum: "189", juan: "216 o 189", aluminiosDelUruguay: "PN 0189"}, this.pieces.centralShash.lenght, "Horizontal Frame", this.color, this.pieces.centralShash.quantity);
+
+        const horizontalShash = new Frame(this.serie, {abasur: "N1753", urualum: "191", juan: "205 o 191", aluminiosDelUruguay: "PN 0191"}, this.pieces.horizontalShash.lenght, "Horizontal Frame", this.color, this.pieces.horizontalShash.quantity);
+
+        const screenShash = new Frame(this.serie, {abasur: "E4436", urualum: "2314", juan: "214", aluminiosDelUruguay: "PN 2314"}, {width: this.pieces.screenWidth.lenght, height: this.pieces.screenHeight.lenght}, "Horizontal Frame", this.color, {widthQuantity: this.pieces.screenWidth.quantity, heightQuantity: this.pieces.screenHeight.quantity});
+
+        const glassWidth = {lenght: this.pieces.glassWidth.lenght, quantity: this.pieces.glassWidth.quantity};
+
+        const glassHeight = {lenght: this.pieces.glassHeight.lenght, quantity: this.pieces.glassHeight.quantity};
+
+        this.frames = {horizontalFrame: horizontalFrame, verticalFrame: verticalFrame, lateralShash: lateralShash, centralShash: centralShash, horizontalShash: horizontalShash, screenShash: screenShash}
+
+        this.glass = {glassWidth: glassWidth, glassHeight: glassHeight};
+
+    }
+
+    
+
+}
+
+#init(){
+    this.framing();
+
+}
+
+getFrames(){
+    this.#init();
+    return this.frames;
+}
+
+getGlass(){
+    this.#init();
+    return this.glass;
+}
+
+
 
 }
