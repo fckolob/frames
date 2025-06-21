490d6ce74690fdaec6f4dde3399c480fc5eb2769
fckolob <131497920+fckolob@users.noreply.github.com>
2025-06-20 21:19:45 -0300

    form added

--- frames/src/main.js
+++ frames/src/main.js
@@ -1,24 +1,38 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
+
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Frames</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+   <form class="form-element" action="post">
+  <label for="serie">Serie</label>
+  <select class="form-input" name="serie" id="serie">
+    <option value="serie20">Serie 20</option>
+    <option value="serie25">Serie 25</option>
+  </select>
+  <label for="color">Color</label>
+  <select class="form-input" name="color" id="color">
+    <option value="anodizado">Anodizado</option>
+    <option value="anolok">Anolok</option>
+    <option value="blanco">Blanco</option>
+    <option value="imitacionMadera">Imitacion Madera</option>
+    <option value="pintadoNegro">Pintado de Negro</option>
+  </select>
+  <label for="vidrio">Vidrio</label>
+  <select class="form-input" name="vidrio" id="vidrio">
+    <option value="simple">Vidrio Simple</option>
+    <option value="dvh">Vidrio DVH</option>
+  </select>
+  <label for="width">Width</label>
+  <input id="width" name="width" type="text">
+  <label for="height">Height</label>
+  <input id="height" name="height" type="text">
+  <label for="quantity">Quantity</label>
+  <input id="quantity" name="quantity" type="number"> 
+  
+  <input class="form-submit" value="Add Opening" id="add-opening" name="add-opening" type="submit">
+</form> 
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+
